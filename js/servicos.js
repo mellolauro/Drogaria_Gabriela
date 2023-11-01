3e2a4b5d3c62b8c5bfc6a17d267dec9394537e98
mellolauro <lauromello@hotmail.com>
2023-10-31 21:39:38 -0300

    ajuste_dialog

--- js/servicos.js
+++ js/servicos.js
@@ -1,13 +1,6 @@
 const openModalButton = document.querySelector('.fVSzUS');
-const closeModalButton = document.querySelector("#close-modal");
-const modal = document.querySelector("#modal");
-const fade = document.querySelector("#fade");
+const modal = document.querySelector("dialog")
 
-const toggleModal = () => {
-    modal.classList.toggle("hide");
-    fade.classList.toggle("hide");
-};
-
-[openModalButton, closeModalButton, fade].forEach((el) => {
-    el.addEventListener("click", () => toggleModal());
-})
+openModalButton.onclick = function () {
+    modal.showModal()
+}
