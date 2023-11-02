e2096fd683fe19694f0cc1947cc95bf47170c0bf
mellolauro <lauromello@hotmail.com>
2023-11-02 18:34:19 -0300

    atualização no modal

--- js/servicos.js
+++ js/servicos.js
@@ -1,6 +1,33 @@
-const openModalButton = document.querySelector('.fVSzUS');
-const modal = document.querySelector("dialog")
+function abrirtetra() {
+    const modal = document.getElementById('janela-tetra')
+    modal.classList.add('abrir')
 
-openModalButton.onclick = function () {
-    modal.showModal()
+    modal.addEventListener('click', (e) => {
+        if (e.target.id == 'fechar' || e.target.id == 'janela-tetra') {
+            modal.classList.remove('abrir')
+        }
+    })
 }
+
+function abrirfluenza() {
+    const modal = document.getElementById('janela-influenza')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if (e.target.id == 'fechar' || e.target.id == 'janela-influenza') {
+            modal.classList.remove('abrir')
+        }
+    })
+}
+
+function abrirherpes() {
+    const modal = document.getElementById('janela-herpes')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if (e.target.id == 'fechar' || e.target.id == 'janela-herpes') {
+            modal.classList.remove('abrir')
+        }
+    })
+}
+
