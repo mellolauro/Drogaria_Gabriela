bfa65ddcd45e7dc45dd1b9ece4b86a6fea2c9b66
mellolauro <lauromello@hotmail.com>
2023-11-12 21:54:54 -0300

    correção agendamento

--- js/agendamento.js
+++ js/agendamento.js
@@ -1,1 +1,7 @@
+const accordion = document.querySelector('[data-js="accordion"]')
 
+accordion.addEventListener('click', e => {
+    const accordionHeardId = e.target.dataset.accordionHeader
+    const accordionItemToBeOpened = document.querySelector('[data-accordion-body="${accordionHeardId}"]')
+    console.log(accordionItemToBeOpened)
+})
